40f5e720d45b63fcdf4f275875b9149b0c2e44b7
Shikha <sj023279@gmail.com>
2024-07-27 19:45:16 +0530

    change in cardmodal

--- frontend/src/components/NoteCards/CardModal.jsx
+++ frontend/src/components/NoteCards/CardModal.jsx
@@ -61,29 +61,29 @@ function CardModal({ operationModal, setOperationModal, operation }) {
                         onChange={(e) => setData({ content: e.target.value })}
                       />
                     </div>
+                    <div className="bg-gray-50 py-3 sm:flex sm:flex-row-reverse">
+                      <button
+                        type="button"
+                        onClick={() => {
+                          setOperationModal(false);
+                          dispatch(setData(data));
+                        }}
+                        className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
+                      >
+                        Submit
+                      </button>
+                      <button
+                        type="button"
+                        data-autofocus
+                        onClick={() => setOperationModal(false)}
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      >
+                        Cancel
+                      </button>
+                    </div>
                   </form>
                 </div>
               </div>
-            </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                onClick={() => {
-                  setOperationModal(false);
-                  dispatch(setData(data));
-                }}
-                className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
-              >
-                Submit
-              </button>
-              <button
-                type="button"
-                data-autofocus
-                onClick={() => setOperationModal(false)}
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-              >
-                Cancel
-              </button>
             </div>
           </DialogPanel>
         </div>
